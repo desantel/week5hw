1cd83d51ff6e56e93cd5b4e04ecb7b015f79d298
Elizabeth De Santis <ecdesantis21@gmail.com>
2021-04-27 17:53:18 -0400

    update readme and javascript

--- Assets/javaScript.js
+++ Assets/javaScript.js
@@ -1,27 +1,40 @@
-
-$(".saveBtn").click(function (e) {
-    e.preventDefault();
-    let input = document.getElementsByClassName("description").value;
-    localStorage.setItem("planner", input);
-    let userInput = localStorage.getItem("planner");
-})
-
 let date = moment();
+let dailyPlan = [];
+
 $("#currentDay").text(date.format("dddd, MMMM Do YYYY, H:mm"));
 
-let timeId = [8,9,10,11,12,13,14,15,16,17,18];
+document.addEventListener("DOMContentLoaded", function () {
+    let savedDay = localStorage.getItem("planner");
+    if (savedDay !== null) { dailyPlan = savedDay }
+    console.log(dailyPlan)
+})
 
+let timeId = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 function colorBg() {
     for (let i = 0; i < timeId.length; i++) {
-    if (timeId[i] == moment().format("H")) {
-        document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightgreen")}
+        if (timeId[i] == moment().format("H")) {
+            document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightgreen")
+        }
         else if (timeId[i] < moment().format("H")) {
-            document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightblue")}
-            else {
-                document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightgray")}
-    } 
+            document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightblue")
+        }
+        else {
+            document.getElementById("hour" + timeId[i]).setAttribute("style", "background-color: lightgray")
+        }
+    }
 
 }
+
+// function storeSearch(plan) {
+//     dailyPlan.push(plan)
+//     console.log(dailyPlan)
+//     localStorage.setItem("planner", JSON.stringify(dailyPlan));
+//     for (let i = 0; i < dailyPlan.length; i++) {
+        
+//     }
+// }
+
+document.getElementById("saveBtn").addEventListener('Click', storeSearch(plan))
 
 colorBg()
--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # week5scheduler
 
-Clock appears on the screen and is in real time
-There are hourly spaces for a user to type into
-There is a save button for each hour spot, as in the [image](./hw/webpage.png)
-The user should be able to type into each field, I was unable to make this work
-The time slots should change color dependent on the relationship between the actual time and current hour.  I was unable to make this work.
-[image](./hw/inspect.png)
+## Table of Contents
+
+*[Description](#description)
+
+*[Usage](#usage)
+
+*[Contribution](#contribute)
+
+*[Contact Me](#Contact-Information)
+  
+## Description
+A day schedular for events and tasks
+   
+## Usage
+Type meetings or to-do in appropriate times.  Click the save button to save information to space. ![image](./Assets/webpage.png)  The future hours, current hour, and previous hours are all different colors.  
+
+
+## Contribution
+None
+
+## Contact-Information
+[Github Profile](https://github.com/desantel)
+[Personal Website](https://desantel.github.io/DeSantisPortfolioPage/portfolio.html)
